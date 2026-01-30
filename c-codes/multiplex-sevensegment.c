5d158ce1d0ebd99d239502e451f6c0b0bdca4fb7
ramlaksh2501 <ramlaksh2501@gmail.com>
2026-01-30 09:08:00 +0530

    mux sevenseg

---
+++ c-codes/multiplex-sevensegment.c
@@ -0,0 +1,27 @@
+/*
+ * File:   mux_seven_lib.c
+ * Author: ramlaksh
+ *
+ * Created on 27 January, 2026, 5:53 PM
+ */
+
+
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF      // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF   
+#define _XTAL_FREQ 20000000
+#include <xc.h>
+#include"/home/ramlaksh/learnings/pic-library/DSP.h"
+void main(void) {
+    TRISB=0x00;
+    TRISC&=~(0x07);
+    PORTB=0;
+    PORTC=0;
+    while(1){mux_seven(145);}//refer the header for the function code
+    return;
+}
